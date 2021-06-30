e1a47462c5598fb8241876dc18eabab89360a6a3
kunwa <kunwa@LAPTOP-EAUVMLVF>
2021-06-30 14:01:40 +0530

    changes

--- newproject/src/newpackage/Exceldata1.java
+++ newproject/src/newpackage/Exceldata1.java
@@ -8,19 +8,55 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class Exceldata1 {
-	public static void main(String[] args) throws IOException
+	
+	public String filedata() throws IOException
 	{
+		String data = null;
 		File src=new File("C:\\Exceldata\\TestData.xlsx");
 		FileInputStream fls=new FileInputStream(src);
 		XSSFWorkbook wb=new XSSFWorkbook(fls);
 		XSSFSheet sheet1=wb.getSheetAt(0);
 		int rowcount=sheet1.getLastRowNum();
+		//int colcount=sheet1.getRow(1).getLastCellNum();
 		System.out.println("Total number of row is "+rowcount);
+		//System.out.println("Total number of row is "+colcount);
 		for(int i=0;i<rowcount+1;i++)
 		{
-			String data=sheet1.getRow(i).getCell(0).getStringCellValue();
-			System.out.println("Data is "+data);
-		}
+			
+				data=sheet1.getRow(i).getCell(0).getStringCellValue();
+				System.out.println("Data is "+data);
+				
+		
+	}
+	
+		
 		wb.close();
+		
+		return data;
+	}
+	public String filedata1() throws IOException
+	{
+		String data1 = null;
+		File src=new File("C:\\Exceldata\\TestData.xlsx");
+		FileInputStream fls=new FileInputStream(src);
+		XSSFWorkbook wb=new XSSFWorkbook(fls);
+		XSSFSheet sheet1=wb.getSheetAt(0);
+		int rowcount=sheet1.getLastRowNum();
+		//int colcount=sheet1.getRow(1).getLastCellNum();
+		System.out.println("Total number of row is "+rowcount);
+		//System.out.println("Total number of row is "+colcount);
+		for(int i=0;i<rowcount+1;i++)
+		{
+			
+				data1=sheet1.getRow(i).getCell(1).getStringCellValue();
+				System.out.println("Data is "+data1);
+				
+		
+	}
+	
+		
+		wb.close();
+		
+		return data1;
 	}
 }
--- newproject/src/newpackage/MyClass.java
+++ newproject/src/newpackage/MyClass.java
@@ -1,7 +1,9 @@
 package newpackage;
 
 
+import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -10,30 +12,33 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MyClass {
-public static void main(String[] args) throws InterruptedException
+public static void main(String[] args) throws InterruptedException, IOException
 {
+	Exceldata1 ex=new Exceldata1();
+	String value=ex.filedata();
+	String value1=ex.filedata1();
 	System.setProperty("webdriver.chrome.driver", "C:\\Selenium Driver\\chromedriver.exe");
 	WebDriver driver=new ChromeDriver();
 	driver.manage().window().maximize();
 	driver.get("https://www.makemytrip.com/");
 	driver.findElement(By.xpath("//span[text()='From']")).click();
-	driver.findElement(By.xpath("//input[@placeholder='From']")).sendKeys("Pune");	
-	Thread.sleep(3000);
+	driver.findElement(By.xpath("//input[@placeholder='From']")).sendKeys(value);	
+	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	driver.findElement(By.xpath("//input[@placeholder='From']")).sendKeys(Keys.ARROW_DOWN);
 	driver.findElement(By.xpath("//input[@placeholder='From']")).sendKeys(Keys.ENTER);
 	driver.findElement(By.xpath("//span[text()='To']")).click();	
-	driver.findElement(By.xpath("//input[@placeholder='To']")).sendKeys("Mumbai");
-	Thread.sleep(3000);
+	driver.findElement(By.xpath("//input[@placeholder='To']")).sendKeys(value1);
+	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	driver.findElement(By.xpath("//input[@placeholder='To']")).sendKeys(Keys.ARROW_DOWN);
 	driver.findElement(By.xpath("//input[@placeholder='To']")).sendKeys(Keys.ENTER);
-	Thread.sleep(3000);
+	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	driver.findElement(By.xpath("//a[text()='Search']")).click();
-	driver.findElement(By.xpath("//input[@title=\"Mon, Jun 21, 2021\"]")).click(); 
-	Thread.sleep(3000);
+	driver.findElement(By.xpath("//div[@aria-label=\"Thursday, 1 July 2021\"]")).click(); 
+	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	driver.findElement(By.xpath("//span[@role=\"button\" and @aria-label=\"Next Month\"]")).click(); 
-	Thread.sleep(3000);
+	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	driver.findElement(By.xpath("//div[@aria-label=\"Friday, 20 August 2021\"]")).click();
-	Thread.sleep(3000);
+	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	int i=0;
 	List <WebElement> buttons=driver.findElements(By.xpath("//span[text()='View Prices']"));
 	for(WebElement we:buttons)
@@ -45,7 +50,7 @@ public static void main(String[] args) throws InterruptedException
 		we.click();
 	}
 	}
-	Thread.sleep(3000);
+	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	int j=0;
 	List <WebElement> buttons1=driver.findElements(By.xpath("//button[@class=\"button corp-btn latoBlack buttonPrimary fontSize13 \"]"));
 	for(WebElement we1:buttons1)
